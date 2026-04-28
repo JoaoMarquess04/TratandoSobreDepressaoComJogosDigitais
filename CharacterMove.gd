2f2e025f19b47c584081f355e3e072742fd16e69
GuiKastro01 <guicastroalv79@gmail.com>
2026-04-28 16:41:20 -0300

    update-character-animation-lighthouse

--- CharacterMove.gd
+++ CharacterMove.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const  SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 
 #ConfigDash 
 const DASH_SPEED = 700.0
@@ -55,7 +55,7 @@ func _physics_process(delta: float) -> void:
 		#Gravidade
 		if not is_on_floor():
 			velocity += get_gravity() * delta
-			sprite_2d.animation = "Jumping"
+			sprite_2d.animation = "jumping"
 
 		#Jump
 		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
