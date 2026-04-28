a892a11807107ff360308447cba73ee97f780555
GuiKastro01 <guicastroalv79@gmail.com>
2026-04-28 01:37:39 -0300

    starter-character-application

---
+++ CharacterMove.gd
@@ -0,0 +1,98 @@
+extends CharacterBody2D
+
+const  SPEED = 200.0
+const JUMP_VELOCITY = -400.0
+
+#ConfigDash 
+const DASH_SPEED = 700.0
+const DASH_TIME = 0.2
+const DASH_VERTICAL_MULTIPLIER = 0.6
+
+#RespawnSystem
+const FALL_LIMIT = 1000.0
+ 
+var is_dashing = false
+var dash_timer = 1.0
+var dash_direction = Vector2.ZERO
+var can_dash = true
+
+var respawn_position: Vector2
+
+@onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+func _ready():
+	#salvar posição inicial
+	respawn_position = global_position
+
+func _physics_process(delta: float) -> void:
+	
+	#verificar queda
+	if global_position.y > FALL_LIMIT:
+		respawn()
+
+	#dash reset
+	if is_on_floor():
+		can_dash = true
+
+	#dash input
+	if Input.is_action_just_pressed("Dash") and can_dash:
+		start_dash()
+
+	#DashSystem
+	if is_dashing:
+		dash_timer -= delta
+		
+		var dash_velocity = dash_direction * DASH_SPEED
+		dash_velocity.y *= DASH_VERTICAL_MULTIPLIER
+		
+		velocity = dash_velocity
+		
+		if dash_timer <= 0:
+			is_dashing = false
+	
+	else:
+		#Normal Movement
+		#Gravidade
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			sprite_2d.animation = "Jumping"
+
+		#Jump
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+
+		#HorMove
+		var direction := Input.get_axis("ui_left", "ui_right")
+		
+		if direction:
+			velocity.x = direction * SPEED
+			sprite_2d.animation = "running"
+		else:
+			velocity.x = move_toward(velocity.x, 0, 20)
+			sprite_2d.animation = "default"
+
+	move_and_slide()
+
+	sprite_2d.flip_h = velocity.x < 0
+
+#DashFunc
+func start_dash():
+	is_dashing = true
+	dash_timer = DASH_TIME
+	can_dash = false
+
+	var input_dir = Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
+	)
+
+	if input_dir == Vector2.ZERO:
+		input_dir = Vector2(-1, 0) if sprite_2d.flip_h else Vector2(1, 0)
+
+	dash_direction = input_dir.normalized()
+
+#RespawnFunc
+func respawn():
+	global_position = respawn_position
+	velocity = Vector2.ZERO
+	is_dashing = false
