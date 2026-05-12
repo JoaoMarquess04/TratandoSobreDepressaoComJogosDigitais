c75572cee3ae8d538b824cb309ee43ddc594c63f
JoaoMarquess04 <jjoaofilipepm@gmail.com>
2026-05-12 00:20:32 -0300

    Configuração de Transição de cena

--- scene_transition_1.gd
+++ scene_transition_1.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+# @export permite definir o valor da variável pelo Inspetor da cena.
+@export var next_scene: String
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +15,8 @@ func _process(delta: float) -> void:
 
 
 func _on_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	# Verifica se o objeto que colidiu com a área tem o nome de classe "CharachterBody2D"
+	if body is CharacterBody2D:
+	# Realiza a troca de cena.
+		get_tree().change_scene_to_file.call_deferred(next_scene)
+	
