c5f4fddbf9d870dfc4161c09b3313f6648c9fb68
Joao Pinheiro <jjoaofilipepm@gmail.com>
2026-05-26 02:03:47 -0300

    funcionalidade de troca de fases adicionada

--- Cenas/scene_transition_2.gd
+++ Cenas/scene_transition_2.gd
@@ -13,4 +13,4 @@ func _on_body_entered(body: Node2D) -> void:
 	# Verifica se o objeto que entrou é um CharacterBody2D
 	if body is CharacterBody2D:
 		# Troca para a cena desejada !!!!!!!!! 
-		get_tree().change_scene_to_file.call_deferred("res://Cenas/CenaFarol.tscn")
+		get_tree().change_scene_to_file.call_deferred("res://Cenas/NiveisFarol/Nivel-1/NivelAndar1.tscn")
