1d93bac5dcbd5c76e449af19d7557364e098e54b
GuiKastro01 <guicastroalv79@gmail.com>
2026-08-13 02:00:51 -0300

    funcao-botao-v2-timer-passivo

---
+++ botaoAtivo.gd
@@ -0,0 +1,45 @@
+class_name BotaoAtivo
+extends Area2D
+
+#Variaveis de entrada para Sprite
+@export var textura_normal: Texture2D
+@export var textura_pressionado: Texture2D
+#Variavel para entrada de temporizador
+@export var tempo_para_resetar: float = 20.0
+#Variavel botao secundario(passivo)
+@export var botao2: Area2D
+
+#Variaveis Gerais
+var player_perto = false
+var botao_pressionado = false
+var botao_confirmado = false
+
+func _ready():
+	$Sprite2D.texture = textura_normal
+
+func _process(delta):
+	if player_perto and Input.is_action_just_pressed("interagir") and not botao_pressionado and not botao_confirmado:
+		#pressionado
+		botao_pressionado = true
+		$Sprite2D.texture = textura_pressionado
+		#temporizado
+		await get_tree().create_timer(tempo_para_resetar).timeout
+		#confirmado
+		if not botao_confirmado:
+			botao_pressionado = false
+			$Sprite2D.texture = textura_normal
+
+#Entrada
+func _on_body_entered(body):
+	if body.name == "CharacterBody2D":
+		player_perto = true
+
+#Saida
+func _on_body_exited(body):
+	if body.name == "CharacterBody2D":
+		player_perto = false
+
+#Chamada do botao passivo quando pressionado
+func confirmar():
+	if botao_pressionado and not botao_confirmado:
+		botao_confirmado = true
--- botaoPassivo.gd
+++ botaoPassivo.gd
@@ -1,18 +1,24 @@
 extends Area2D
 
+#variaveis dos sprites
 @export var textura_normal: Texture2D
 @export var textura_pressionado: Texture2D
+#variavel de entrada do botao ativo
+@export var botao1: BotaoAtivo
 
+#variaveis gerais
 var player_perto = false
-var botao_pressionado = false
+var pressionado = false
 
 func _ready():
 	$Sprite2D.texture = textura_normal
 
 func _process(delta):
-	if player_perto and Input.is_action_just_pressed("interagir"):
-		botao_pressionado = !botao_pressionado
-		$Sprite2D.texture = textura_pressionado if botao_pressionado else textura_normal
+	if player_perto and Input.is_action_just_pressed("interagir") and not pressionado:
+		if botao1.botao_pressionado and not botao1.botao_confirmado:
+			pressionado = true
+			$Sprite2D.texture = textura_pressionado
+			botao1.confirmar()
 
 func _on_body_entered(body):
 	if body.name == "CharacterBody2D":
